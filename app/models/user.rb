98f9432124b3e2e21741703ceb5d3f2c16ef39b0
depippo <jdepippo@gmail.com>
2016-07-16 19:45:09 -0400

    Category feature tests passing

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :carts
+  has_many :orders
+  has_one :current_cart, class: Cart
+
+  def create_current_cart
+    cart = carts.create
+    self.current_cart_id = cart.id
+    save
+  end
+  
+end
